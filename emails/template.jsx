684f765b0460c75f2b03ef18c4e0d0d6d9ca8db8
MyselfAkashYadav <akashyadav2927@gmail.com>
2025-04-06 23:02:50 +0530

    :sparkles: generate monthly insight using ai llms

--- emails/template.jsx
+++ emails/template.jsx
@@ -5,8 +5,10 @@ import * as React from "react";
 
 export default function EmailTemplate({
     userName="",
-    type="budget-alert",
-    data={ },
+    type="monthly-report",
+    data={
+      
+     },
 
 })
 
